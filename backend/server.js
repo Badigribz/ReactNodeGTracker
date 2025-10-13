39dfe56eb32812a1b34ab488ad85ea550b94cd4a
Gribz <gribzbadi@gmail.com>
2025-10-13 10:24:38 +0300

    Added server.js and dependencies like cors,dotenv,express mongoose

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// backend/server.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// simple test route
+app.get("/", (req, res) => {
+  res.send("🔥 Backend is running!");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
